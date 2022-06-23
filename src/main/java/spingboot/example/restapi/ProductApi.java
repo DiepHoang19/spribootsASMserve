4d37c07af66c44ccfda612761bba794c2ce0f282
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-06-23 09:55:04 +0700

    update

--- src/main/java/spingboot/example/restapi/ProductApi.java
+++ src/main/java/spingboot/example/restapi/ProductApi.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/v1/products")
 @Validated
+@CrossOrigin("*")
 public class ProductApi {
     @Autowired
     ProductService productService;
